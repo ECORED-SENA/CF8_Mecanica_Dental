3cffa975e727f983cb7166c1998c207014dd788f
AndresDiazPinto <andresvis565@gmail.com>
2022-10-12 02:18:28 -0500

    Se carga AD
    Se vincula AD

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -12,12 +12,12 @@
         .titulo-segundo
           h2 Características de las prótesis fijas
         p.mb-4 Identificar las características de las prótesis fijas para aplicarlas en la fabricación, teniendo en cuenta los protocolos de fabricación. 
-        .tarjeta.actividad.p-3
+        .tarjeta.bgr_22.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Falso o verdadero
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
                 
